dfcb2b71ac0dcf944941c6ec6f26abdbede5a164
Abdel Hakim Bensiali <bensialih@gmail.com>
2024-02-20 18:03:55 +0000

    cargo fmt and removed unused code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod file;
 mod helpers;
-use file::{check_file_size, settings, rotate_file};
+use file::{check_file_size, rotate_file, settings};
 use std::{thread, time::Duration};
 
 fn main() {
@@ -15,11 +15,11 @@ fn main() {
         if file_size < data.bytes() {
             data = settings(Some(settings_file));
             println!("current size {file_size:?} --- {}", data.bytes());
-    
+
             file_size = check_file_size(file_location).unwrap();
-    
+
             println!("current size after {file_size:?}");
-    
+
             thread::sleep(Duration::from_millis(100));
         } else {
             rotate_file(String::from(file_location));
@@ -27,6 +27,4 @@ fn main() {
         }
         // break 'checker;
     }
-
-    
 }
